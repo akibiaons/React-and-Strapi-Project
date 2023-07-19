461c85507b279446daab09ed748442bffd0285f4
Arthur Fernandez <arthur@snooget.com>
2023-07-18 18:17:23 -0700

    Added Apollo client setup and provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, UNSAFE_DataRouterContext } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 
 // Page and layout imports below
 import Header from './Components/Header';
@@ -7,16 +9,24 @@ import Homepage from './Pages/Homepage';
 import ReviewDetails from './Pages/ReviewDetails';
 import Category from './Pages/Category';
 
+// Apollo client for new connection...
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
-    <div className="App">
-      <Header/>
-        <Routes>
-          <Route exact path="/" element={<Homepage />}/>
-          <Route path="/details/:id" element={<ReviewDetails />}/>
-          <Route path="/category/:id" element={<Category />}/>
-        </Routes>
-    </div>
+      <div className="App">
+        <Header/>
+        <ApolloProvider client={client}>
+          <Routes>
+            <Route exact path="/" element={<Homepage />}/>
+            <Route path="/details/:id" element={<ReviewDetails />}/>
+            <Route path="/category/:id" element={<Category />}/>
+          </Routes>
+        </ApolloProvider>
+      </div>
   );
 }
 
