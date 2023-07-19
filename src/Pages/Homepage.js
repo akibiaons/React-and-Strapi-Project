76de197e8c2a08e5e0a15eb708eef2bcbce52e98
Arthur Fernandez <arthur@snooget.com>
2023-07-18 19:07:03 -0700

    changed from useFetch to GraphQl

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import useFetch from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import {useQuery, gql} from '@apollo/client';
+
+const REVIEWS = gql`
+query GetReviews {
+  reviews {
+    data {
+      id,
+      attributes {
+        title,
+        rating,
+        body
+      }
+    }
+  }
+}
+` 
 
 export default function Homepage() {
-  const { loading, error, data } = useFetch('http://localhost:1337/api/reviews');
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error...</p>;
+  const {loading, error, data} = useQuery(REVIEWS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error...</p>
 
   console.log(data); // Check the structure of the reviews data in the console
 
   return (
     <div>
-      {data.data.map(review => (
+      {data.reviews.data.map(review => (
         <div key={review.id} className="review-card">
           <div className="rating">{review.attributes.rating}</div>
           <h2>{review.attributes.title}</h2>
