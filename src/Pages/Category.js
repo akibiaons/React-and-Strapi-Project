3444a9dfe4fbbeeda424c1b89649fd3cd1c383ba
Arthur Fernandez <arthur@snooget.com>
2023-07-20 07:54:54 -0700

    Added category query

--- src/Pages/Category.js
+++ src/Pages/Category.js
@@ -1,60 +1,70 @@
-import React from 'react'
-import {useQuery, gql} from '@apollo/client';
-import { useParams, Link } from 'react-router-dom';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { useParams, Link } from "react-router-dom";
 
 const CATEGORY = gql`
-  query($id: ID!) {
+  query GetCategory($id: ID!) {
     category(id: $id) {
       data {
         id
-          attributes {
-            name
-              reviews {
-                data {
-                  attributes {
-                    title
-                    rating
-                    body
+        attributes {
+          name
+          reviews {
+            data {
+              id
+              attributes {
+                rating
+                title
+                body
+                categories {
+                  data {
+                    id
+                    attributes {
+                      name
+                    }
                   }
-           }
+                }
+              }
+            }
+          }
         }
       }
     }
   }
-}
+`;
 
-`
-
-export default function Category() {
-  const { id } = useParams()
+function Category() {
+  const { id } = useParams();
   const { loading, error, data } = useQuery(CATEGORY, {
-    variables: { id: id }
-  })
+    variables: { id: id },
+  });
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error...</p>
-
-  const category = data.category.data[0].attributes;
-  const reviews = data.category.data[0].attributes.reviews.data;
-
-
-    console.log(data)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( </p>;
 
   return (
-    <div>
-      <h2>{category.name}</h2>
-      {reviews.map(review => (
-        <div key={review.id} className="review-card">
-          <div className="rating">{review.attributes.rating}</div>
-          <h2>{review.attributes.title}</h2>
-
-          <small>console list</small>
-
-          <p>{review.attributes.body.substring(0, 200)}...</p>
-
-          <Link to={`/details/${review.id}`}>Read More</Link>
-        </div>
-      ))}
+    <div className="category__wrapper">
+      <h2>{data.category.data.attributes.name}</h2>
+      {data.category.data.attributes.reviews.data.map((review) => {
+        return (
+          <div key={review.id} className="review__card">
+            <div className="card__rating">{review.attributes.rating}</div>
+            <h2 className="review__title">{review.attributes.title}</h2>
+            <div className="card__categories flex-row">
+              {review.attributes.categories.data.map((c) => (
+                <span key={c.id}>{c.attributes.name}</span>
+              ))}
+            </div>
+            <p className="review__body">
+              {review.attributes.body.substring(0, 200)}...
+            </p>
+            <Link to={`/details/${review.id}`}>Read More</Link>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
+
+export default Category;
+
